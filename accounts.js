3a6c1cf5b8c541f5d13231a30798a08c2fed45af
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-03-14 20:45:26 -0400

    Created user object

--- accounts.js
+++ accounts.js
@@ -1,1 +1,26 @@
 "use strict"
+// keeps the different users
+
+class user {
+   constructor(name, choice){
+       this.name = name;
+       this.choice = choice;
+   }
+   picksRock(){
+       this.choice = rock;
+   }
+   picksPaper(){
+       this.choice = paper;
+   }
+   picksScissors(){
+       this.choice = scissors;
+   }
+   picksLizard(){
+       this.choice = lizard;
+   }
+   picksSpock(){
+       this.choice = spock;
+   }
+};
+
+
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 "use strict"
+
+// created AI user using mat.floor.random to pick randomly between the choices
+// create a function for the user to pick between the choices
+// create a function that compares the two choices
+// keep score
+
