bb462266c15480341d9be5f10391927dba537143
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-03-15 23:11:52 -0400

    Worked on refactoring after instructor reviewed the code

--- accounts.js
+++ accounts.js
@@ -3,8 +3,7 @@
 const prompt = require('prompt-sync')();
 
 class User {
-   constructor(name){
-       this.name = name;
+   constructor(){
        this.choice = null;
        this.score = 0;
    }
@@ -25,6 +24,65 @@ class User {
    }
 };
 
+class Human extends User {
+    constructor(name){
+        super();
+        this.name = name;
+        this.choice = '';
+    }
+    chooseGesture(){
+        this.choice = prompt(`Enter ${this.name}'s choice here -> `)
+        switch (this.choice){
+             case '0':
+             this.picksRock();
+             break;
+             case '1':
+             this.picksPaper();
+             break;
+             case '2':
+             this.picksScissors();
+             break;
+             case '3':
+             this.picksLizard();
+             break;
+             case '4':
+             this.picksSpock();
+             break;
+             default:
+             console.log('')
+             console.log('Whoops! Try again, you need to enter a number between 0 and 4')
+             this.chooseGesture();
+             break;
+             }
+   }
+}
+class AI extends User {
+    constructor(){
+        super();
+        this.name = 'AI'; 
+        this.choice = '';
+    }
+    chooseGesture(){
+        this.choice = Math.floor(Math.random()*5)
+        switch (this.choice){
+             case 0:
+             this.picksRock();
+             break;
+             case 1:
+             this.picksPaper();
+             break;
+             case 2:
+             this.picksScissors();
+             break;
+             case 3:
+             this.picksLizard();
+             break;
+             case 4:
+             this.picksSpock();
+             break;
+             }}
+}
+
 
 
 
@@ -34,3 +92,5 @@ let choices = [
 
 module.exports.User = User;
 module.exports.choices = choices;
+module.exports.Human = Human;
+module.exports.AI = AI;
--- game.js
+++ game.js
@@ -1,24 +1,27 @@
 "use strict"
-
-//TODO list:
-//Incorporate inheratance
-
 const prompt = require('prompt-sync')();
 const {User} = require('./accounts');
+const {Human} = require('./accounts');
+const {AI} = require('./accounts');
 const {choices} = require('./accounts');
-let numOfPlayers;
-let playerOne;
-let playerTwo;
-let AI;
-let gameCount = 0;
+
 class Game {
      
      constructor (){
-          this.AI = new User('AI');
+          this.numOfPlayers = null;
+          this.playerOne = null;
+          this.playerTwo = null;
+          this.gameCount = 1;
      }
      start(){
           this.intro();
           this.chooseNumberOfPlayers();
+          this.instructions();
+          this.listGestureOptions();
+          this.playerOne.chooseGesture();
+          this.playerTwo.chooseGesture();
+          this.checkForWinner();
+          this.displayWinner();
      }
      intro(){  
           console.log(''); 
@@ -44,12 +47,12 @@ class Game {
           console.log('');
      }
      chooseNumberOfPlayers(){
-          numOfPlayers = prompt('How many people are playing? 1 or 2?');
-          if (numOfPlayers === '2'){
-               this.twoPlayerMode();
-          } else if (numOfPlayers === '1'){
-               this.singlePlayerMode();
-          }else if (numOfPlayers !== ('1' || '2')){
+          this.numOfPlayers = prompt('How many people are playing? 1 or 2?');
+          if (this.numOfPlayers === '2'){
+               this.settingPlayers();
+          } else if (this.numOfPlayers === '1'){
+               this.settingPlayers();
+          }else if (this.numOfPlayers !== ('1' || '2')){
                console.log('');
                console.log('Must select either "1" or "2"');
                console.log('');
@@ -63,84 +66,6 @@ class Game {
           console.log('Choose 3 for LIZARD');
           console.log('Choose 4 for SPOCK');
      }
-     chooseGesture(){
-          this.playerOne.choice = prompt(`Enter ${this.playerOne.name}'s choice here -> `)
-          switch (this.playerOne.choice){
-               case '0':
-               this.playerOne.picksRock();
-               break;
-               case '1':
-               this.playerOne.picksPaper();
-               break;
-               case '2':
-               this.playerOne.picksScissors();
-               break;
-               case '3':
-               this.playerOne.picksLizard();
-               break;
-               case '4':
-               this.playerOne.picksSpock();
-               break;
-               default:
-               console.log('')
-               console.log('Whoops! Try again, you need to enter a number between 0 and 4')
-               this.chooseGesture();
-               break;
-               }
-     }
-     chooseGestureTwo(){
-          this.playerTwo.choice = prompt(`Enter ${this.playerTwo.name}'s choice here -> `)
-          switch (this.playerTwo.choice){
-               case '0':
-               this.playerTwo.picksRock();
-               break;
-               case '1':
-               this.playerTwo.picksPaper();
-               break;
-               case '2':
-               this.playerTwo.picksScissors();
-               break;
-               case '3':
-               this.playerTwo.picksLizard();
-               break;
-               case '4':
-               this.playerTwo.picksSpock();
-               break;
-               default:
-               console.log('')
-               console.log('Whoops! Try again, you need to enter a number between 0 and 4')
-               this.chooseGestureTwo();
-               break;
-               }     
-               console.log('')
-               console.log(`${this.playerOne.name}'s choice was -> ${this.playerOne.choice}`)
-               console.log(`${this.playerTwo.name}'s choice was -> ${this.playerTwo.choice}`)
-               console.log('')
-     }
-     chooseGestureAI(){
-          this.AI.choice = Math.floor(Math.random()*5)
-          switch (this.AI.choice){
-               case 0:
-               this.AI.picksRock();
-               break;
-               case 1:
-               this.AI.picksPaper();
-               break;
-               case 2:
-               this.AI.picksScissors();
-               break;
-               case 3:
-               this.AI.picksLizard();
-               break;
-               case 4:
-               this.AI.picksSpock();
-               break;
-               }
-               console.log('')
-               console.log(`${this.playerOne.name}'s choice was -> ${this.playerOne.choice}`)
-               console.log(`${this.AI.name}'s choice was -> ${this.AI.choice}`)
-               console.log('')
-          }
      checkForWinner(){
           if ((this.playerOne.choice === choices[0]) && (this.playerTwo.choice === choices[2] || this.playerTwo.choice === choices[3])){
                this.playerOne.score++;
@@ -153,104 +78,45 @@ class Game {
           } else if ((this.playerOne.choice === choices[4]) && (this.playerTwo.choice === choices[2] || this.playerTwo.choice === choices[0])){
                this.playerOne.score++;
           } else if(this.playerOne.choice === this.playerTwo.choice){
-               this.playerOne.score++;
-               this.playerTwo.score++;
           } else {this.playerTwo.score++;}
-          this.displayScoresTwoPlayers();
-          gameCount++;
-          if (gameCount <3){
-          this.chooseGesture();
-          this.chooseGestureTwo();
+          this.displayPlayerSelections();
+          this.displayScores();
+          this.gameCount++;
+          if (this.gameCount <4){
+          this.playerOne.chooseGesture();
+          this.playerTwo.chooseGesture();
           this.checkForWinner();
-          }else if (gameCount >= 3){
+          }else if (this.gameCount >= 4){
                if (this.playerOne.score === this.playerTwo.score){
                     console.log('')
                     console.log('Tie Game!')
                     console.log('Play again to settle the score')
                     console.log('')
-                    gameCount = 0;
+                    this.gameCount = 1;
                     this.playerOne.score = 0;
                     this.playerTwo.score = 0;
-                    this.chooseGesture();
-                    this.chooseGestureTwo();
+                    this.playerOne.chooseGesture();
+                    this.playerTwo.chooseGesture();
                     this.checkForWinner();
-               }
-             
+               }      
           }
      } 
-     checkForWinnerSinglePlayer(){
-           if ((this.playerOne.choice === choices[0]) && (this.AI.choice === choices[2] || this.AI.choice === choices[3])){
-               this.playerOne.score++;
-          } else if ((this.playerOne.choice === choices[1]) && (this.AI.choice === choices[0] || this.AI.choice === choices[4])){
-               this.playerOne.score++;
-          } else if ((this.playerOne.choice === choices[2]) && (this.AI.choice === choices[1] || this.AI.choice === choices[3])){
-               this.playerOne.score++;
-          } else if ((this.playerOne.choice === choices[3]) && (this.AI.choice === choices[4] || this.AI.choice === choices[1])){
-               this.playerOne.score++;
-          } else if ((this.playerOne.choice === choices[4]) && (this.AI.choice === choices[2] || this.AI.choice === choices[0])){
-               this.playerOne.score++;
-          } else if(this.playerOne.choice === this.AI.choice){
-               this.playerOne.score++;
-               this.AI.score++;
-          } else {this.AI.score++;}
-          this.displayScoresSinglePlayer();
-          gameCount++;
-          if (gameCount <3){
-          this.chooseGesture();
-          this.chooseGestureAI();
-          this.checkForWinnerSinglePlayer();
-          }else if (gameCount >= 3){
-               if (this.playerOne.score === this.AI.score){
-                    console.log('')
-                    console.log('Tie Game!')
-                    console.log('Play again to settle the score')
-                    console.log('')
-                    gameCount = 0;
-                    this.chooseGesture();
-                    this.chooseGestureAI();
-                    this.checkForWinnerSinglePlayer();
-               }
-               
-          }
+     settingPlayers(){
+          if (this.numOfPlayers === '2'){
+               this.playerOne = new Human(prompt("Please enter player one's name ")); 
+               this.playerTwo = new Human(prompt("Please enter player two's name "));
+               console.log('Two Player mode selected');
+          } else if (this.numOfPlayers === '1'){
+               this.playerOne = new Human(prompt("Please enter player one's name "));
+               this.playerTwo = new AI('AI');
+               console.log('Single Player mode selected')
+          } 
      }
-     twoPlayerMode(){
-          console.log('Two Player mode selected');
-          this.playerOne = new User(prompt("Please enter player one's name ")); 
-          this.playerTwo = new User (prompt("Please enter player two's name "));
-          this.instructions();
-          this.listGestureOptions();
-          this.chooseGesture();
-          this.chooseGestureTwo();
-          this.checkForWinner();
-          this.displayWinnerTP();
-     }
-     singlePlayerMode(){
-          console.log('Single Player mode selected')
-          
-          this.playerOne = new User(prompt("Please enter player one's name ")); 
-          this.instructions();
-          this.listGestureOptions();
-          this.chooseGesture();
-          this.chooseGestureAI();
-          this.checkForWinnerSinglePlayer();
-          this.displayWinnerSP();
-     }
-     displayScoresSinglePlayer(){
-          console.log(`${this.playerOne.name}'s score -> ${this.playerOne.score}`);
-          console.log(`${this.AI.name}'s score -> ${this.AI.score}`); 
-     }
-     displayScoresTwoPlayers(){
+     displayScores(){
           console.log(`${this.playerOne.name}'s score -> ${this.playerOne.score}`);
           console.log(`${this.playerTwo.name}'s score -> ${this.playerTwo.score}`);
      }
-     displayWinnerSP(){
-          if (this.playerOne.score < this.AI.score){
-               console.log(` Sorry ${this.AI.name} WINS`)
-          } else if (this.playerOne.score > this.AI.score){
-               console.log(`${this.playerOne.name} WINS!`)
-          }
-     }
-     displayWinnerTP(){
+     displayWinner(){
           if (this.playerOne.score < this.playerTwo.score){
                console.log('')
                console.log(`${this.playerTwo.name} WINS!`)
@@ -261,7 +127,12 @@ class Game {
                console.log('')
           }
      }
+     displayPlayerSelections(){
+          console.log('')
+          console.log(`${this.playerOne.name}'s choice was -> ${this.playerOne.choice}`)
+          console.log(`${this.playerTwo.name}'s choice was -> ${this.playerTwo.choice}`)
+          console.log('')
+     }
 }
-
 module.exports.Game = Game;
 
